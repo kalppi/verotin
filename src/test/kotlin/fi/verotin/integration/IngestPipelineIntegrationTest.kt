8443231c5f6d15bbb89e8c54f55ff237d0a22ee9
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2026-04-28 09:11:58 +0300

    integration test fixes

--- src/test/kotlin/fi/verotin/integration/IngestPipelineIntegrationTest.kt
+++ src/test/kotlin/fi/verotin/integration/IngestPipelineIntegrationTest.kt
@@ -19,6 +19,8 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -39,6 +41,14 @@ class IngestPipelineIntegrationTest {
             .withDatabaseName("verotin_test")
             .withUsername("verotin")
             .withPassword("verotin")
+
+        @DynamicPropertySource
+        @JvmStatic
+        fun configureProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url") { postgres.jdbcUrl }
+            registry.add("spring.datasource.username") { postgres.username }
+            registry.add("spring.datasource.password") { postgres.password }
+        }
     }
 
     @Autowired
@@ -110,7 +120,7 @@ class IngestPipelineIntegrationTest {
 
     @Test
     fun `chunking respects window and overlap parameters`() {
-        val longText = "Lorem ipsum dolor sit amet. ".repeat(50)  // ~1400 chars
+        val longText = "Lorem ipsum dolor sit amet. ".repeat(100)  // ~2800 chars, exceeds DEFAULT_WINDOW (1500)
 
         val chunks = chunkingService.slidingWindows(longText)
 
